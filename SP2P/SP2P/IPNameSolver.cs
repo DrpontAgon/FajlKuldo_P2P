8ab9ac0c91ff1600506237087b418344ca0f8013
Ákos Tóth <toth_aki@freemail.hu>
2017-08-21 22:49:38 +0200

    IPNameSolver kikommentelve, kommentek javítása IPLogger-ben

--- SP2P/SP2P/IPNameSolver.cs
+++ SP2P/SP2P/IPNameSolver.cs
@@ -7,12 +7,42 @@ using System.Text;
 
 namespace SP2P
 {
+    /// <summary>
+    /// 
+    /// Az IPNameSolver célja, hogy elmentsen a bejövő IP címekhez a felhasználó által
+    /// megadott neveket és, hogy ezekből névfeloldást csináljon a következő alkalommal
+    /// az adott IP esetén. Egy IP cím csak egy nevet tárolhat, azonban ugyan ahhoz a névhez
+    /// több IP cím is tartozhat. Tárolása helytakarékosság céljából a számítógépen
+    /// alapértelmezett ANSI kódolást használ, ami fix 1 bájt hosszú. Ezenfelül az IP binárisan van elmentve,
+    /// ezért külső szemlélőnek egy cím 4 karakternek felel meg.
+    /// 
+    /// Általános használata:
+    /// 
+    /// IPAddress x = bármilyen IP cím;
+    /// string y = bármilyen név;
+    /// var z;
+    /// var zs;
+    /// 
+    /// x.GetIPOrNamesIfExist(out string y_out);
+    /// y.GetIPOrNamesIfExist(out IPAddress x_out);
+    /// 
+    /// x.SetIPOrNames(out string y_out);
+    /// 
+    /// x.RemoveIPOrName();
+    /// y.RemoveIPOrName();
+    /// 
+    /// z = GetAllIPAndNames();
+    /// zs = GetAllIPAndNamesSorted();
+    /// 
+    /// </summary>
     public static class IPNameSolver
     {
         /// <summary>
+        /// 
         /// Statikus konstruktor, program indításakor fut le és beállítja az alapértelmezett
         /// helyét (és nevét) a fájlnak. Az alapértelmezett hely azt a mappát jelenti
         /// ahol a futtatható állomány is megtalálható.
+        /// 
         /// </summary>
         static IPNameSolver()
         {
@@ -21,6 +51,21 @@ namespace SP2P
 
         public static string Fpath { get; set; }
 
+        /// <summary>
+        /// 
+        /// A függvény neve magáért beszél, az a változat ahol a bemenet egy IP cím és a kimenet a
+        /// hozzá tartozó név, ha létezik, és ennek a sorszáma a fájlon belül. A sikerességről is kapunk
+        /// egy értéket.
+        /// 
+        /// Mivel csak belső műveleteknél szükséges a sorszám, és mivel minden függvény ami
+        /// a fájllal dolgozik valamilyen formában, itt kell hogy megvalósítva legyen,
+        /// ezért a program egyéb részeihez nem szükséges a sorszám, így ez a verziója függvénynek privát.
+        /// 
+        /// </summary>
+        /// <param name="ip"> A megadott IP cím </param>
+        /// <param name="name"> A hozzá tartozó név </param>
+        /// <param name="index"> A hozzá tartozó sorszám </param>
+        /// <returns> Logikai érték, igaz, ha szerepel az IP cím a fájlban </returns>
         private static bool GetIPOrNamesIfExist(this IPAddress ip, out string name, out int index)
         {
             name = string.Empty;
@@ -43,6 +88,21 @@ namespace SP2P
             return name != string.Empty;
         }
 
+        /// <summary>
+        /// 
+        /// A függvény neve magáért beszél, az a változat ahol a bemenet egy név és a kimenet a
+        /// hozzá tartozó IP címek, ha létezik, és ennek a sorszámai a fájlon belül. A sikerességről is kapunk
+        /// egy értéket.
+        /// 
+        /// Mivel csak belső műveleteknél szükséges a sorszám, és mivel minden függvény ami
+        /// a fájllal dolgozik valamilyen formában, itt kell hogy megvalósítva legyen,
+        /// ezért a program egyéb részeihez nem szükséges a sorszám, így ez a verziója függvénynek privát.
+        /// 
+        /// </summary>
+        /// <param name="name"> A megadott név </param>
+        /// <param name="ips"> A hozzá tartozó IP címek </param>
+        /// <param name="indices"> A hozzá tartozó sorszámok </param>
+        /// <returns> Logikai érték, igaz, ha szerepel a név a fájlban </returns>
         private static bool GetIPOrNamesIfExist(this string name, out IPAddress[] ips, out int[] indices)
         {
             List<IPAddress> l_ips = new List<IPAddress>();
@@ -68,16 +128,43 @@ namespace SP2P
             return name != string.Empty;
         }
 
+        /// <summary>
+        /// 
+        /// A két paraméteres változat, különbség a három paraméteres megfelelőjéhez képest,
+        /// hogy nem kapjuk vissza a sorszámokat és ezért ez publikus is.
+        /// 
+        /// </summary>
+        /// <param name="ip"> A megadott IP cím </param>
+        /// <param name="name"> A hozzá tartozó név </param>
+        /// <returns> Logikai érték, igaz, ha szerepel az IP cím a fájlban </returns>
         public static bool GetIPOrNamesIfExist(this IPAddress ip, out string name)
         {
             return GetIPOrNamesIfExist(ip, out name, out int unused);
         }
 
+        /// <summary>
+        /// 
+        /// A két paraméteres változat, különbség a három paraméteres megfelelőjéhez képest,
+        /// hogy nem kapjuk vissza a sorszámokat és ezért ez publikus is.
+        /// 
+        /// </summary>
+        /// <param name="name"> A megadott név </param>
+        /// <param name="ips"> A hozzá tartozó IP címek </param>
+        /// <returns> Logikai érték, igaz, ha szerepel a név a fájlban </returns>
         public static bool GetIPOrNamesIfExist(this string name, out IPAddress[] ips)
         {
             return GetIPOrNamesIfExist(name, out ips, out int[] unused);
         }
 
+        /// <summary>
+        /// 
+        /// Segéd függvény, feladata létrehozni a fájlban szerepló sor formátumát
+        /// a megadott IP címből és névből.
+        /// 
+        /// </summary>
+        /// <param name="ip"> A megadott IP cím </param>
+        /// <param name="name"> A megadott név </param>
+        /// <returns> A fájl formátumának megfelelő egy sor </returns>
         private static string IPAndNameLine(IPAddress ip, string name)
         {
             byte[] b_arr = ip.GetAddressBytes();
@@ -89,6 +176,18 @@ namespace SP2P
             return new string(c_arr) + name;
         }
 
+        /// <summary>
+        /// 
+        /// A függvény feladata hogy a megadott IP címhez hozzárendelje a megadott nevet.
+        /// Ha az IP cím már létezik akkor az felülíródik, ha nem akkor hozzáadódik a fájlhoz
+        /// az új bejegyzés.
+        /// 
+        /// Ebből csak ez az egy változat van, ha szükségessé válik akkor implementálva lesz
+        /// a másik változat is.
+        /// 
+        /// </summary>
+        /// <param name="ip"> A megadott IP cím </param>
+        /// <param name="name"> A megadott név </param>
         public static void SetIPOrName(this IPAddress ip, string name)
         {
             if (GetIPOrNamesIfExist(ip, out string unused, out int index))
@@ -104,6 +203,13 @@ namespace SP2P
             }
         }
 
+        /// <summary>
+        /// 
+        /// A függvény neve magáért beszél, eltávolítja az IP címet és a hozzá
+        /// tartozó nevet a fájlból.
+        /// 
+        /// </summary>
+        /// <param name="ip"> A megadott IP cím </param>
         public static void RemoveIPOrName(this IPAddress ip)
         {
             if (GetIPOrNamesIfExist(ip, out string unused, out int index))
@@ -114,6 +220,13 @@ namespace SP2P
             }
         }
 
+        /// <summary>
+        /// 
+        /// A függvény neve magáért beszél, eltávolítja a nevet és a hozzá
+        /// tartozó összes IP címet a fájlból.
+        /// 
+        /// </summary>
+        /// <param name="name"> A megadott név </param>
         public static void RemoveIPOrName(this string name)
         {
             if (GetIPOrNamesIfExist(name, out IPAddress[] unused))
@@ -124,7 +237,14 @@ namespace SP2P
             }
         }
 
-        public static Dictionary<IPAddress, string> GetAllIPNames()
+        /// <summary>
+        /// 
+        /// A függvény neve magáért beszél, visszaadja az összes fájlban szereplő bejegyzést
+        /// kulcs-érték párokban, abban a sorrendben ahogy az a fájlban szerepel.
+        /// 
+        /// </summary>
+        /// <returns> Az összes bejegyzés kulcs-érték párokban </returns>
+        public static Dictionary<IPAddress, string> GetAllIPAndNames()
         {
             Dictionary<IPAddress, string> dictionary = new Dictionary<IPAddress, string>();
             IPAddress line_ip;
@@ -139,14 +259,53 @@ namespace SP2P
             return dictionary;
         }
 
-        public static SortedDictionary<IPAddress, string> GetAllIPNamesSorted()
+        /// <summary>
+        /// 
+        /// A függvény neve magáért beszél, visszaadja az összes fájlban szereplő bejegyzést
+        /// kulcs-érték párokban, IP cím szerint növekvő sorrendben.
+        /// 
+        /// </summary>
+        // <returns> Az összes bejegyzés kulcs-érték párokban, sorrendben </returns>
+        public static SortedDictionary<IPAddress, string> GetAllIPAndNamesSorted()
         {
-            return new SortedDictionary<IPAddress, string>(GetAllIPNames(), new IPAddressComparer());
+            return new SortedDictionary<IPAddress, string>(GetAllIPAndNames(), new IPAddressComparer());
         }
     }
 
+    /// <summary>
+    /// 
+    /// Egyéb IPAddress-t kiegészítő osztály, célja tárolni az összes többi szükséges függvényt
+    /// vagy metódust, ami alapértelmezetten nem szerepel az IPAddress osztályban.
+    /// Előfordulhat hogy ez az osztály másik fájlba fog költözni.
+    /// 
+    /// Általános használata:
+    /// 
+    /// IPAddress x = bármilyen IP cím;
+    /// 
+    /// x.KiegeszitoMetodus();
+    /// var0 = x.KiegeszitoMetodus(param1, param2, ..., paramn);
+    /// 
+    /// </summary>
     public static class IPAddressExtender
     {
+        /// <summary>
+        /// 
+        /// A függvény feladata hogy visszadja a korrekt pozitív egész számot ami reprezentálja az IP címet.
+        /// Memóriakezelési és átváltási okokból a szám típúsok a memóriában bájtonként
+        /// fordítva vannak tárolva, és ezt a beépített típusfordítók nem veszik figyelembe,
+        /// ezzel azt elérve, hogy az IP címet reprezentáló szám nincs helyes sorrendben.
+        /// 
+        /// Példa:
+        /// 
+        /// helyes sorrend:    Beépített fordítás:    Ez a függvény:
+        ///     0.0.0.1             16777216                1
+        ///     0.0.0.2             33554432                2
+        ///     0.0.1.0              65536                 256
+        ///     1.0.0.0                1                 16777216
+        /// 
+        /// </summary>
+        /// <param name="ip"> A megadott IP cím </param>
+        /// <returns> Az IP címet reprezentáló </returns>
         public static uint ToUInt32(this IPAddress ip)
         {
             byte[] x = ip.GetAddressBytes();
@@ -155,8 +314,35 @@ namespace SP2P
         }
     }
 
+    /// <summary>
+    /// 
+    /// IPAddress-t összehasonlító osztály, az általános összehasonlító osztályból származtatva,
+    /// egyetlen kötelezően implementált függvénnyel. Célja az olyan függvények kielégítése,
+    /// ahol szükséges egy összehasonlító, és az összehasonlítandó adatok IPAddress típusúak.
+    /// Előfordulhat hogy ez az osztály másik fájlba fog költözni.
+    /// 
+    /// </summary>
     public class IPAddressComparer : Comparer<IPAddress>
     {
+        /// <summary>
+        /// 
+        /// A kötelezően implementált függvény, célja összehasonlítási eredményt
+        /// adni a az első IP cím kapcsolatáról a másodikhoz képest.
+        /// Az összehasonlításhoz azokat a pozitív egész számokat hasonlítjuk össze,
+        /// a számokhoz tartozó beépített összehasonlítóval, amik helyesen
+        /// reprezentálják az IP címet.
+        /// 
+        /// </summary>
+        /// <param name="ip1"> Az első megadott IP cím </param>
+        /// <param name="ip2"> A második megadott IP cím </param>
+        /// <returns>
+        /// 
+        /// Visszaadja az IP címek kapcsolatát:
+        /// -1 ha az első cím megelőzi a másodikat  (kisebb)
+        ///  0 ha a címek azonosak                  (egyenlő)
+        ///  1 ha az első cím követi a másodikat    (nagyobb)
+        ///  
+        /// </returns>
         public override int Compare(IPAddress ip1, IPAddress ip2)
         {
             return ip1.ToUInt32().CompareTo(ip2.ToUInt32());
