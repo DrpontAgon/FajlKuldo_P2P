fab267c2f0e8297d9cc2e061012d51d5bea899c3
DrpontAgon <benseviktor@hotmail.com>
2017-08-22 23:58:04 +0200

    Server és Client osztályok

---
+++ SP2P/SP2P/Server_Client.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.IO.Ports;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SP2P
+{
+    public class Server
+    {
+        public TcpListener Tcp = null;
+        public Server(IPAddress localIP, int port = 55585)
+        {
+            Tcp = new TcpListener(localIP, port);
+        }
+    }
+    public class Client
+    {
+        private TcpClient client = null;
+        public NetworkStream ns = null;
+        public Client(TcpClient socket)
+        {
+            client = socket;
+            ns = socket.GetStream();
+        }
+        public void Send(string filepath)
+        {
+            
+        }
+    }
+}
