6cb0f44e3a4e63bb69b44a823a503eef04db15ab
DrpontAgon <benseviktor@hotmail.com>
2018-06-02 11:38:31 +0200

    FileReceiverForm

---
+++ SP2P/SP2P/FileReceiverForm.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace SP2P
+{
+    partial class FileReceiverForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.AcceptButton = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.FileName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.FileSize = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.CheckAll = new System.Windows.Forms.Button();
+            this.CheckNone = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(284, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Melyik fájl(oka)t kívánja fogadni?";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.CancelButton.Location = new System.Drawing.Point(197, 396);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(75, 23);
+            this.CancelButton.TabIndex = 1;
+            this.CancelButton.Text = "Mégse";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            this.CancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // AcceptButton
+            // 
+            this.AcceptButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.AcceptButton.Location = new System.Drawing.Point(116, 396);
+            this.AcceptButton.Name = "AcceptButton";
+            this.AcceptButton.Size = new System.Drawing.Size(75, 23);
+            this.AcceptButton.TabIndex = 1;
+            this.AcceptButton.Text = "Fogadás";
+            this.AcceptButton.UseVisualStyleBackColor = true;
+            this.AcceptButton.Click += new System.EventHandler(this.AcceptButton_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.CheckBoxes = true;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.FileName,
+            this.FileSize});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listView1.LabelEdit = true;
+            this.listView1.Location = new System.Drawing.Point(13, 56);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(259, 334);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.AfterLabelEdit += new System.Windows.Forms.LabelEditEventHandler(this.listView1_LabelEdited);
+            // 
+            // FileName
+            // 
+            this.FileName.Text = "Fájlnév";
+            this.FileName.Width = 165;
+            // 
+            // FileSize
+            // 
+            this.FileSize.Text = "Fájlméret";
+            this.FileSize.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.FileSize.Width = 61;
+            // 
+            // CheckAll
+            // 
+            this.CheckAll.Location = new System.Drawing.Point(13, 27);
+            this.CheckAll.Name = "CheckAll";
+            this.CheckAll.Size = new System.Drawing.Size(126, 23);
+            this.CheckAll.TabIndex = 3;
+            this.CheckAll.Text = "Mind kijelölése";
+            this.CheckAll.UseVisualStyleBackColor = true;
+            this.CheckAll.Click += new System.EventHandler(this.CheckAll_Click);
+            // 
+            // CheckNone
+            // 
+            this.CheckNone.Location = new System.Drawing.Point(146, 27);
+            this.CheckNone.Name = "CheckNone";
+            this.CheckNone.Size = new System.Drawing.Size(126, 23);
+            this.CheckNone.TabIndex = 4;
+            this.CheckNone.Text = "Összes kijelölés törlése";
+            this.CheckNone.UseVisualStyleBackColor = true;
+            this.CheckNone.Click += new System.EventHandler(this.CheckNone_Click);
+            // 
+            // FileReceiverForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 431);
+            this.ControlBox = false;
+            this.Controls.Add(this.CheckNone);
+            this.Controls.Add(this.CheckAll);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.AcceptButton);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FileReceiverForm";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Fájl(ok) fogadása";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button CancelButton;
+        private System.Windows.Forms.Button AcceptButton;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader FileName;
+        private System.Windows.Forms.ColumnHeader FileSize;
+        private System.Windows.Forms.Button CheckAll;
+        private System.Windows.Forms.Button CheckNone;
+    }
+}
---
+++ SP2P/SP2P/FileReceiverForm.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SP2P
+{
+    public partial class FileReceiverForm : Form
+    {
+        public (string FileName, ulong Size)[] ReceiveFiles { get; private set; }
+        public FileReceiverForm((string,ulong)[] files)
+        {
+            InitializeComponent();
+            ReceiveFiles = files;
+            for (int i = 0; i < files.Length; i++)
+            {
+                listView1.Items.Add(files[i].Item1).SubItems.Add(files[i].Item2+ " MB");
+            }
+        }
+
+        private void listView1_LabelEdited(object sender, LabelEditEventArgs e)
+        {
+            ReceiveFiles[e.Item].FileName = e.Label;
+        }
+
+        private void AcceptButton_Click(object sender, EventArgs e)
+        {
+            List<(string,ulong)> files = new List<(string, ulong)>();
+            for (int i = 0; i < listView1.Items.Count; i++)
+            {
+                if (listView1.Items[i].Checked)
+                {
+                    files.Add((listView1.Items[i].Text, ReceiveFiles[i].Size));
+                }
+            }
+
+            ReceiveFiles = files.ToArray();
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem item in listView1.Items)
+            {
+                item.Checked = false;
+            }
+
+            ReceiveFiles = null;
+        }
+
+        private void CheckAll_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem item in listView1.Items)
+            {
+                item.Checked = true;
+            }
+        }
+
+        private void CheckNone_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem item in listView1.Items)
+            {
+                item.Checked = false;
+            }
+        }
+    }
+}
